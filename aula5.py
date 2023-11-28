77c698f9b18718da2ebdb0d1c085ad557341bf52
dsbbrasil <146891847+dsbbrasil@users.noreply.github.com>
2023-11-28 18:58:11 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,10 @@
+# quadrado = lambda numero : numero ** 2
+
+# print(quadrado(6))
+
+
+numeros = lambda numero : "POSITIVO" if numero >= 0 else "NEGATIVO"
+
+n = float(input("Escolha um numero"))
+
+print(numeros(n))
---
+++ ques1.py
@@ -0,0 +1,5 @@
+numero1 = lambda numero : " par" if numero % 2 == 0 else "Impar"
+
+x= int(input("ESCOLHA UM NÚMERO:"))
+
+print(numero1(x))
